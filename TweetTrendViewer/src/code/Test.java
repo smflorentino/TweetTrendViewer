027f62f5b58be885f9170e08109839c01d0c0f9b
Alyssa Buzzard <alyssabu@buffalo.edu>
2013-02-23 13:22:17 -0500

    Displays the top tags/mentions/words from given input file as a word cloud with font size of each item being proportional to its rank.  I ended up using the code I had already started because I've been working with OpenCloud. Complete Except: 	-integrate Scott's code to allow user to specify file 	-allow user to specify the number of top items to display (right now set to 20) 	-add some css to make output more visually appealing

--- TweetTrendViewer/src/code/Test.java
+++ TweetTrendViewer/src/code/Test.java
@@ -1,5 +1,12 @@
 package code;
 
+import javax.swing.*;
+
 public class Test {
 
+	public static void main(String args[]){
+		TweetCloud t = new TweetCloud();
+		
+	}
+	
 }
--- TweetTrendViewer/src/code/TweetCloud.java
+++ TweetTrendViewer/src/code/TweetCloud.java
@@ -1,18 +1,40 @@
 package code;
 
-import org.mcavallo.opencloud.Cloud;
-import org.mcavallo.opencloud.Tag;
+import java.util.ArrayList;
+
+import javax.swing.JEditorPane;
+
+import org.mcavallo.opencloud.*;
+import org.mcavallo.opencloud.formatters.*;
+
 
 public class TweetCloud {
 	private Cloud cloud;
+	private ArrayList<Tag> tags;
+	
+//	Get the top tags (number of top tags defined in ResultReader
+//	Add the top tags to the cloud
 	public TweetCloud(){
-		cloud = new Cloud(); // create cloud cloud.setMaxWeight(38.0); // max font size
-		cloud.setMaxTagsToDisplay(20); // at most 20 tags displayed
+		System.out.println("Welcome to TweetCloud");
+		cloud = new Cloud(); // create cloud 
+		cloud.setMaxWeight(50.0); // max font size
+//		TODO: change to be variable path name
+		ResultReader r = new ResultReader("inputs/tags.txt");
+		r.getWordCounts();
+		tags = r.getTags();
+		cloud.addTags(tags);
+		for(Tag t:tags){
+			t.setLink("");
+	//		t.normalize(tags.get(0).getScore());
+		}
+		HTMLFormatter h = new HTMLFormatter();
+		h.addCss("body","font-size","20");
+		Visualizer v = new Visualizer(h.html(cloud));//this is a jframe
+		v.start();
 	}
 	
-	public void addTag(String word, int weight){
-		Tag tag = new Tag(word, weight);
-		
-	}
+//	public void addTag(String word, int weight){
+//		Tag tag = new Tag(word, weight);	
+//	}
 
 }
